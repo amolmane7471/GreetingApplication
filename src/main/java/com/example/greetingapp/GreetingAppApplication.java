730aa5e63fdc569d8bee8603431ff6d5ecaa7d2b
amolmane7471 <maneamol3233@gmail.com>
2022-11-04 19:41:47 +0530

    [Amol] : added UC1 Using greeting Controller return JSON for different Http methods

--- src/main/java/com/example/greetingapp/GreetingAppApplication.java
+++ src/main/java/com/example/greetingapp/GreetingAppApplication.java
@@ -8,7 +8,6 @@ public class GreetingAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GreetingAppApplication.class, args);
-        System.out.println("welcome to greeting application");
     }
 
 }
